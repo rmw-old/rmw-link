c71fcde11173bf5670b533cad266cbcf1b5cdbd4
anyone <anyone@renwu.cool>
2022-05-09 03:19:54 +0800

    master

--- docker/run.sh
+++ docker/run.sh
@@ -4,6 +4,8 @@ DIR=$(dirname $(realpath "$0"))
 cd $DIR
 set -ex
 docker run -it --rm \
-  -v $DIR/../rust:/root/rust\
+  -v $DIR/../rust:/root/rust \
+  -v $DIR/cache/cargo/git:/opt/cargo/git \
+  -v $DIR/cache/cargo/registry:/opt/cargo/registry \
   rmw-link-build \
   /bin/sh -c "cd /root/rust && /bin/zsh"
