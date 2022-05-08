974e30e2c83ecb6306d022d1f614f82ca52fe3aa
gcxfd <i@rmw.link>
2022-05-09 02:17:59 +0800

    🔶

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,4 @@
 DIR=$(dirname $(realpath "$0"))
 cd $DIR
 set -ex
-docker run -it --rm rmw-link-build -v $DIR/rust:/root/rust /bin/zsh -c "cd /root/rust"
+docker run -it --rm rmw-link-build -v $DIR/../rust:/root/rust /bin/zsh -c "cd /root/rust"
