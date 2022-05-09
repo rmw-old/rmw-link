b0a4d45ae66b89a9f01402e50b1cb4732fcd86cb
anyone <anyone@renwu.cool>
2022-05-10 03:43:39 +0800

    master

--- docker/make.sh
+++ docker/make.sh
@@ -6,7 +6,7 @@ set -ex
 
 plugin=$HOME/.docker/cli-plugins
 
-if [ -e "$plugin/docker-buildx" ]; then
+if [ ! -e "$plugin/docker-buildx" ]; then
 mkdir -p $plugin
 buildx=$plugin/docker-buildx
 wget https://github.com/docker/buildx/releases/download/v0.8.2/buildx-v0.8.2.linux-amd64 -O $buildx
