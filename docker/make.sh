4ac62bc9f85456ab4f6c2544ffff40aa2c90c64a
anyone <anyone@renwu.cool>
2022-05-10 03:35:47 +0800

    master

--- docker/make.sh
+++ docker/make.sh
@@ -4,5 +4,14 @@ DIR=$(dirname $(realpath "$0"))
 cd $DIR
 set -ex
 
+plugin=$HOME/.docker/cli-plugins
+
+if [ -e "$plugin/docker-buildx" ]; then
+mkdir -p $plugin
+buildx=$plugin/docker-buildx
+wget https://github.com/docker/buildx/releases/download/v0.8.2/buildx-v0.8.2.linux-amd64 -O $buildx
+chmod +x $buildx
+fi
+
 docker buildx build -t rmwl/build .
 docker push rmwl/build
