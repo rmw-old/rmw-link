9872fa0537bb7986ede8d1795a3ef0e6eff203a4
anyone <anyone@renwu.cool>
2022-05-10 04:01:15 +0800

    master

--- docker/build.sh
+++ docker/build.sh
@@ -4,11 +4,11 @@ DIR=$(dirname $(realpath "$0"))
 cd $DIR
 set -ex
 
-docker run --name rmwl/build \
+docker run --name rmw-link-build \
   -v $DIR/../rust:/root/rust \
   -v $DIR/cache/rust/git:/opt/rust/git \
   -v $DIR/cache/rust/registry:/opt/rust/registry \
-  rmw-link-build \
+  rmwl/build \
   /bin/zsh -c "cd /root/rust && rustup default nightly && rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu && ./build.xsh"
 
 docker cp rmw-link-build:/root/rust/target/x86_64-unknown-linux-gnu/release/rmw rmw
