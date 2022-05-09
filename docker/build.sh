48a8e2a756e250f4f3f632eae7071315a935d0c0
gcxfd <i@rmw.link>
2022-05-10 01:16:49 +0800

    🔶

--- docker/build.sh
+++ docker/build.sh
@@ -3,6 +3,9 @@
 DIR=$(dirname $(realpath "$0"))
 cd $DIR
 set -ex
-
-docker build -t rmw-link-build .
-./run.sh
+docker run --rm \
+  -v $DIR/../rust:/root/rust \
+  -v $DIR/cache/cargo/git:/opt/cargo/git \
+  -v $DIR/cache/cargo/registry:/opt/cargo/registry \
+  rmw-link-build \
+  /bin/zsh -c "cd /root/rust && ./build.xsh"
